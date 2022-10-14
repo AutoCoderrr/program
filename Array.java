864704b1699953495b41c4c229123917d5fe8e88
AutoCoderrr <68611365+AutoCoderrr@users.noreply.github.com>
2022-10-14 12:44:31 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Array 
+{
+public static void main (String [] args)
+{
+
+
+int[] number={1,2,3,4,5};
+
+
+System.out.print("second highest no is :"+number[3]);
+}
+}
